6ca9ee4ff108d41e61537a4435ce5c0b4a406cc5
RenanSouza <renansouza0122@gmail.com>
2023-02-02 16:03:22 -0300

    if not add a div class 'task'

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 const buttonAdd = document.querySelector(".btn-add");
 const taskInput = document.querySelector(".task-input");
+const tasks = document.querySelector("#tasks");
 buttonAdd.addEventListener('click',()=>{
     if(taskInput.value.length == 0){
         taskInput.placeholder = "Please Enter a Task!";
         // Verficy if input is empty  
+    }else{
+        tasks.innerHTML = `
+            <div class="task">
+                <span id="task-name">
+                    ${taskInput.value}
+                </span>
+                <button class="delete">
+                    <i class="fa-regular fa-circle-xmark"></i>
+                </button>
+            </div>
+        `;
     }
 })
